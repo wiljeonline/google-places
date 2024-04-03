b9d60bd131b7d1191809221bf1dd0c3c6b7c1932
wiljeonline <37470506+wiljeonline@users.noreply.github.com>
2024-04-03 22:23:28 +0200

    Initial commit
    🚀 The initial commit for this class.

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# google-places
+# Wilje Online Google Places 
 Just a simple PHP class we use in our projects for the Google Places API. 
+
+License: Apache 2.0
+
+# Installation 
+## 1. Set up the Google API 
+**Set up the Google Places API** 
+We need to enable the **Places API**. 
+Setup: https://developers.google.com/maps/documentation/places/web-service/cloud-setup
+
+**Get Google Place ID** 
+Get the Google Place ID for the place ID you want to use. 
+
+The easiest option is to use this website: 
+https://developers.google.com/maps/documentation/places/web-service/place-id.
+
+## 2. Set the API keys in your project 
+When you have your API and keys setup, we use roughly one of two types of settings.
+
+### Option 1. Set API keys via PHP Constants (safest)*
+
+Follow these steps: 
+- Set a constant named WO_GOOGLE_PLACE_ID in wp-config.php and fill in your Place ID
+- Set a constant named WO_GOOGLE_PLACES_API_KEY in wp-config.php and fill in your API key
+
+### Option 2. Set API keys via ACF Settings fields (safe enough)
+
+Follow these steps: 
+- Install and active Advanced Custom Fields Pro 
+- Set up two ACF settings fields named wo_google_place_id & wo_google_places_api_key
+- Set them as options in an ACF options page
+- Fill in the Google Place ID and API key in the options pages and save the settings 
+
+## Example usage of the class
+You can now use the class as follows: 
+
+### Get all the place details
+```php
+use WiljeOnline\GooglePlaces;
+
+$googlePlaces = new GooglePlaces;
+$placeDetails = $googlePlaces->getPlaceDetails(); 
+```
+
+### Get the reviews
+```php
+use WiljeOnline\GooglePlaces;
+
+$googlePlaces = new GooglePlaces;
+$reviews = $googlePlaces->getReviews();
+```
+
+### Get the aggregated rating 
+```php
+use WiljeOnline\GooglePlaces;
+
+$googlePlaces = new GooglePlaces;
+$aggregatedRating = $googlePlaces->getAggregatedRating();
+```
+## Usage in your theme 
+You can now get the reviews and aggregated ratings through the WordPress options. 
+The settings are saved as transients and will expire after 24 hours maximum. The class is set up with a cron job that runs twice daily to fetch the new data.
+
+**Get the transients** 
+```php 
+$reviews_transient = get_transient('wo_reviews');
+
+if( $reviews_transient ) { 
+    // Do something with the reviews.
+}
+```
+
+## Authors
+- [@jeroen-mulder](https://www.github.com/jeroen-mulder)
+
+## License
+[Apache 2.0](https://choosealicense.com/licenses/apache-2.0)
---
+++ wiljeonline-google-places.php
@@ -0,0 +1,201 @@
+<?php
+
+/**
+ * Class to get the reviews and aggregated rating from the Google Places API
+ * 
+ * @package Wilje_Online_Starter
+ * 
+ */
+class GooglePlaces
+{
+
+    private $apiKey;
+    private $placeId;
+    private $placeDetails;
+
+    public function __construct()
+    {
+        $this->apiKey = $this->getApiKey();
+        $this->placeId = $this->getPlaceId();
+
+        // Check if the transients exist
+        $reviews_transient = get_transient('wo_reviews');
+        $aggregated_rating_transient = get_transient('wo_aggregated_rating');
+
+        // If the transients do not exist, get the place details
+        if (!$reviews_transient || !$aggregated_rating_transient) {
+            $this->placeDetails = $this->getPlaceDetails();
+        }
+
+        add_action('wp_loaded', [$this, 'register_cron_job']);
+    }
+
+    /**
+     * Register the cron job
+     *
+     * @return void
+     */
+    public function register_cron_job()
+    {
+        if (!wp_next_scheduled('wo_fetch_google_places_data')) {
+            wp_schedule_event(time(), 'twicedaily', 'wo_fetch_google_places_data');
+        }
+
+        add_action('wo_fetch_google_places_data', [$this, 'fetchData']);
+    }
+
+    public function getPlaceDetails()
+    {
+        if (!$this->apiKey || !$this->placeId) {
+            // Throw an exception if the api key or place id is not set
+            throw new Exception('No valid API key or place id found');
+
+            // Is it wise to delete the cron job here?
+            wp_clear_scheduled_hook('wo_fetch_google_places_data');
+        }
+
+        $url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=$this->placeId&key=$this->apiKey&language=nl";
+
+        try {
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            if ($response) {
+                $data = json_decode($response, true);
+
+                if (!isset($data['result'])) {
+                    throw new Exception('No valid place details found');
+                }
+
+                return $data;
+            }
+        } catch (Exception $e) {
+            // Handle the exception
+            error_log(print_r($e->getMessage(), true));
+        }
+    }
+
+    /**
+     * Get the reviews from the transient
+     * If the transient is not set, get the reviews from the Google Places API
+     *
+     * @param  array   $data
+     * @return array
+     */
+    public function getReviews()
+    {
+        $reviews_transient = get_transient('wo_reviews');
+
+        if ($reviews_transient) {
+            return $reviews_transient;
+        }
+
+        if (!isset($this->placeDetails['result']['reviews'])) {
+            return;
+        }
+
+        $reviews_transient = $this->placeDetails['result']['reviews'];
+        set_transient('wo_reviews', $reviews_transient, 60 * 60 * 24);
+        return $reviews_transient;
+    }
+
+    /**
+     * Get the aggregated rating from the transient
+     * If the transient is not set, get the aggregated rating from the Google Places API
+     *
+     * @param  array    $data
+     * @return array
+     */
+    public function getAggregatedRating()
+    {
+        $aggregated_rating_transient = get_transient('wo_aggregated_rating');
+
+        if ($aggregated_rating_transient) {
+            return $aggregated_rating_transient;
+        }
+
+        if (!isset($this->placeDetails['result']['rating']) || !isset($this->placeDetails['result']['user_ratings_total'])) {
+            return;
+        }
+
+        $aggregated_rating_transient = [];
+        if (isset($this->placeDetails['result']['rating'])) {
+            $aggregated_rating_transient['rating'] = $this->placeDetails['result']['rating'];
+        }
+
+        if (isset($this->placeDetails['result']['user_ratings_total'])) {
+            $aggregated_rating_transient['user_ratings_total'] = $this->placeDetails['result']['user_ratings_total'];
+        }
+
+        set_transient('wo_aggregated_rating', $aggregated_rating_transient, 60 * 60 * 24);
+        return $aggregated_rating_transient;
+    }
+
+    /** 
+     * Fetch the data from the Google Places API through a cron job
+     */
+    public function fetchData()
+    {
+        // Delete the transients
+        delete_transient('wo_reviews');
+        delete_transient('wo_aggregated_rating');
+
+        // Fetch the data
+        try {
+            $this->getReviews();
+            $this->getAggregatedRating();
+        } catch (Exception $e) {
+            error_log('Error fetching data: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Get the place id from the options page
+     *
+     * @return  string
+     */
+    private function getPlaceId()
+    {
+        if (!function_exists('get_field')) {
+            return;
+        }
+
+        if (defined('WO_GOOGLE_PLACE_ID')) {
+            return WO_GOOGLE_PLACE_ID;
+        }
+
+        if (!get_field('wo_google_place_id', 'option')) {
+            return false;
+        }
+
+        // Get the place id from the options page
+        return get_field('wo_google_place_id', 'option');
+    }
+
+    /**
+     *  Get the api key from the options page
+     *
+     * @return  string  
+     */
+    private function getApiKey()
+    {
+        if (!function_exists('get_field')) {
+            return;
+        }
+
+        if (defined('WO_GOOGLE_PLACES_API_KEY')) {
+            return WO_GOOGLE_PLACES_API_KEY;
+        }
+
+        if (!get_field('wo_google_places_api_key', 'option')) {
+            return false;
+        }
+
+        // Get the api key from the options page
+        return get_field('wo_google_places_api_key', 'option');
+    }
+}
